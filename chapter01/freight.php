845180bbb9351af249422f3e7618945af2a6037d
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-25 17:21:39 -0400

    Added the code that could replace the while loop using a for loop

--- chapter01/freight.php
+++ chapter01/freight.php
@@ -19,6 +19,15 @@
                           </tr>\n";
                           $distance += 50;
                 }
+
+                # If we re-write the while loop using a for loop we get the following:
+                // for ($distance = 50; $distance <= 250; $distance += 50){
+                //     echo "<tr>
+                //           <td style=\"text-align: right;\">".$distance."</td>
+                //           <td style=\"text-align: right;\">".($distance / 10)."</td>
+                //           </tr>\n";
+                // } 
+                # giving us the same result.
             ?>
         </table>
     
