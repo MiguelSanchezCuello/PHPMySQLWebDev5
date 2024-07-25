542c62291fc56db5e44a363aaa6df4202996bfe5
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-25 18:53:27 -0400

    Added teh new field for the address to the orderform and started the coding to processorder.php

--- chapter02/processorder.php
+++ chapter02/processorder.php
@@ -2,6 +2,24 @@
     // creae short variable names
     $tireqty = (int) $_POST['tireqty'];
     $oilqty = (int) $_POST['oilqty'];
+    $sparkqty = (int) $_POST['sparkqty'];
+    // $address = preg_replace('/\t|\R/',' ',$_POST['address']);
+    $address = $_POST['address'];
+    $document_root = $_SERVER['DOCUMENT_ROOT'];
+    $date = date('H:i, jS F Y');
 ?>
 
 <!DOCTYPE html>
+<html lang="en">
+    <head></head>
+    <body>
+        <?php
+            echo $tireqty."<br />";
+            echo $oilqty."<br />";
+            echo $sparkqty."<br />";
+            echo $address."<br />";
+            echo $document_root."<br />";
+            echo $date."<br />";
+        ?>
+    </body>
+</html>
